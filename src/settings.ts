d0b100bbfabe406c9d277cc3e8affed49b4a596e
Hosinoharu <hosinoharu@qq.com>
2025-12-07 00:33:56 +0800

    语法符号解析太频繁了，增大时间间隔

--- src/settings.ts
+++ src/settings.ts
@@ -13,11 +13,11 @@ export const retry_interval = 200;
 //#endregion
 
 /** 监听文件语法符号的间隔 */
-export const watch_doc_change_interval = 300;
+export const watch_doc_change_interval = 500;
 /** follow cursor 时的 debounce 间隔，要比 `watch_doc_change_interval` 多一点才行
  * 文档更新完成之后才能正确进行鼠标高亮嘛
  */
-export const follow_cursor_interval = 350;
+export const follow_cursor_interval = 550;
 /** follow viewport 时的 debounce 间隔 */
 export const follow_viewport_interval = 200;
 /** 点击符号跳转到位置时，会修改滚动条，为了避免触发 `follow viewport`，
